0b62bfde9153a2a5f85f94aa4c6871e745c73975
flippyshoe <flippyshoe@debian-desktop-snes.lan>
2024-07-16 14:22:33 +0300

    setup now has more steps, README updated accordingly

--- README.md
+++ README.md
@@ -1,31 +1,12 @@
-all of the dotfiles i use between computers\
-only works on linux, windows support in the future (but i dont promise anything)
+### dotfiles - everything *i* need for my computer use
+dotfiles that suit my needs, caters towards linux (and more unix based operating systems in the future)\
+at this moment i make the assertion you are running debian (arch will exist later)\
+(also i have not tested it on debian **based** distros thus i cannot guarantee it will work)
 
-requirements:\
-a linux distribution (preferably arch/debian based)\
-emacs\
-alacritty\
-i3
-
-optional requirements:\
-JetBrainsMono and Terminus nerd fonts (from https://www.nerdfonts.com/font-downloads)
-
-dependencies (debian and derivatives)
-```shell
-## special note abt debian: current alacritty package in debian repos still uses the yml config format
-##                          once it finally updates ill rewrite the config to toml
-apt install emacs alacritty i3 zsh zsh-syntax-highlighting rofi
-```
-
-dependencies (arch and derivatives)
-```shell
-pacman -S emacs alacritty i3 zsh zsh-syntax-highlighting rofi
-```
-
-to automatically add the config files (but dont be surprised when you gotta intervene)
-```shell
+to sort out everything automatically after you cloned the repo
+```bash
 ./setup.sh
 ```
-
-only reason its public is that i wont have to bother logging in every time i wanna use it\
-do whatever you want with it
+be sure to follow every input prompt it gives\
+also it escalates privileges for the package manager so be wary of that\
+check out the source code to make sure nothing is... bad, y'know? 
--- setup.sh
+++ setup.sh
@@ -1,13 +1,71 @@
 #!/bin/bash
 
-## flippyshoe's dotfile repo
-## because im tired of rewriting
+## the one big shell script to rule them all
+## sorts out config files, fonts, themes, etc
+## follow the prompts on screen and good things will happen
 
-# get the source directory of the dotfiles
-# (i.e. the script directory)
-SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+## important symbols:
+## >>> = a stage in the script
+## [E] = an error
+## [!] = attempting to run a privileged command
+## [=] = stage done successfuly
+## [?] = prompt
+## [-] = anything else
+
+echo '>>> starting setup...'
+
+## prerequisite: getting the distrobution, package manager and optionally flatpak
+echo '>>> asserting prerequisites'
+
+if [ -f /etc/*release ]; then
+    OS=$(grep '^ID=' /etc/*release | sed "s/ID=//g")
+    echo "[-] asserting distribution ID is: $OS"
+else
+    echo '[E] missing /etc/os-release - cant figure out which distribution you are in'
+    return
+fi
+
+if [[ $OS == "debian" ]]; then
+    PM="apt"
+fi
+echo "[-] asserting package manager is going to be: $PM"
+
+echo "[=] prerequesites successfuly filled!"
+## stage 1: updating the package manager repos and installing the necessary programs
+echo '>>> stage 1: downloading the necessary packages'
+
+# variadic arg: commands?
+run_privileged_cmd () {
+    cmd=$@
+    echo "[!] running privileged command: $cmd"
+    sudo $cmd
+}
+
+# variadic arg: package names
+get_dependencies () {
+    if [[ $PM == "apt" ]]; then
+	run_privileged_cmd "apt update"
+	run_privileged_cmd "apt install $@"
+    fi
+}
+
+# 2 args: command, message
+run_optional_cmd () {
+    command=$1
+    message=$2
+    read -p "$message (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return
+    $command
+}
+
+get_dependencies emacs alacritty i3 zsh zsh-syntax-highlighting rofi
+
+echo "[=] packages successfuly installed!"
+## stage 2: symbolic links and configurations
+echo ">>> stage 2: setting up symlinks and dotfiles"
 
 # function to create a symlink
+# 2 args: filepath, destination
+# TODO: kinda overcomplicated and gimmicky, redo later
 symlinkFile() {
     name="$(basename "$1")"
     filename="$SCRIPT_DIR/$1"
@@ -19,13 +77,13 @@ symlinkFile() {
     
     if [ ! -L "$destination" ]; then
         if [ -e "$destination" ]; then
-            echo "[ERROR] $destination exists but it's not a symlink. Please fix that manually" && exit 1
+            echo "[E] $destination exists but it's not a symlink. Please fix that manually" && exit 1
         else
             ln -s "$filename" "$destination"
-            echo "[OK] $filename -> $destination"
+            echo "[-] $filename -> $destination"
         fi
     else
-        echo "[WARNING] $filename already symlinked"
+        echo "[E] $filename already symlinked"
     fi
 }
 
@@ -46,3 +104,28 @@ symlinkFile zsh .config
 ## just link the bin dir in the dotfiles folder to there :3
 ## (stupid solution but it works)
 symlinkFile bin .local
+
+echo "[=] configs have been managed!"
+## stage 3: optional dependencies
+echo ">>> stage 3: optional dependencies"
+
+# fonts
+download_fonts () {
+    if [[ ! -f /usr/bin/unzip ]]; then
+	get_dependencies unzip
+    fi
+    
+    mkdir ~/.local/share/fonts && mkdir /tmp/setup-temp
+    ## JetBrainsMon and Terminus fonts
+    wget -P /tmp/setup-temp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Terminus.zip
+    ## unpack them and shove them into ~/.local/share/fonts
+    unzip /tmp/setup-temp/JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono/
+    unzip /tmp/setup-temp/Terminus.zip -d ~/.local/share/fonts/Terminus/
+}
+
+msg="[?] install optional fonts? (verify that the following link is not compromised if you do: https://www.nerdfonts.com/font-downloads)"
+run_optional_cmd "download_fonts" "$msg"
+
+echo ">>> completed stage 3!"
+
+echo ">>> SETUP IS COMPLETE"
